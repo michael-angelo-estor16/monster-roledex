ddac420cdaa2228602f391b1b52f17c7098ec828
michael-angelo-estor16 <michael.angelo.estor16@gmail.com>
2021-08-13 21:28:51 +0800

    done"

--- src/App.js
+++ src/App.js
@@ -7,21 +7,32 @@ class App extends Component {
     super();
 
     this.state = {
-      string: 'Hello Mike',
+      monsters: [
+        {
+          name: 'Luigi',
+        },
+        {
+          name: 'Mario',
+        },
+        {
+          name: 'Moshi',
+        },
+      ],
     };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((data) => console.log(data));
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>{this.state.string}</p>
-
-          <button onClick={() => this.setState({ string: 'Huwaaaaa!' })}>
-            Change string
-          </button>
-        </header>
+        {this.state.monsters.map((monster, index) => (
+          <h1 key={index}>{monster.name}</h1>
+        ))}
       </div>
     );
   }
